7be7b1601fa74210acc028fc4e4d3c36fbe0e6d6
Keith Gilbertson <keith.gilbertson@gmail.com>
2016-05-15 18:10:21 -0400

    Fix Instructions screen so that top of text is visible

---
+++ Re-Resolver/InstructionsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InstructionsViewController.swift
+//  Re-Resolver
+//
+//  Created by Keith Gilbertson on 5/15/16.
+//  Copyright © 2016 Amanda and Keith. All rights reserved.
+//
+
+import UIKit
+
+
+// This class is the controller for the instructions
+// page.
+class InstructionsViewController: UIViewController {
+
+   
+    @IBOutlet weak var textView: UITextView!
+   
+    // Make sure the beginning of the text
+    // is visible in the scroll pane
+    override func viewDidLayoutSubviews() {
+        textView.setContentOffset(CGPoint.zero, animated: false)
+    }
+   
+}
