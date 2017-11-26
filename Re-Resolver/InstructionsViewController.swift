528f7debe52c6c4adb642b7ca52811058beb408f
Keith Gilbertson <keith.gilbertson@gmail.com>
2017-11-26 16:48:50 -0500

    Update information screen with fancy format

--- Re-Resolver/InstructionsViewController.swift
+++ Re-Resolver/InstructionsViewController.swift
@@ -11,26 +11,56 @@ import UIKit
 
 // This class is the controller for the instructions
 // page.
-class InstructionsViewController: UIViewController {
+class InstructionsViewController: UIViewController, UIWebViewDelegate {
 
-   
-    @IBOutlet private weak var textView: UITextView!
-   
-    // Make sure the beginning of the text
-    // is visible in the scroll pane
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        textView.setContentOffset(CGPoint.zero, animated: false)
+    // TODO: UIWebView is deprecated. Replace with WKWebView or
+    // SafariViewController. Note: WKWebView may have problems
+    // when instantiated via storyboards prior to iOS 11.
+    // SafariViewController may (I don't know yet) be difficult
+    // to customize with translucent background rendering to match
+    // the look of the rest of the app.
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    // Load internationalized help text into the webview.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.delegate = self
+        
+        // get the preferred language. This should be more flexible,
+        // but at present the only localizations are English and Spanish.
+        var languageCode = "en"
+        let preferredLocalization = Bundle.main.preferredLocalizations[0]
+        if preferredLocalization.hasPrefix("es")  {
+           languageCode = "es"
+        }
+        
+        if let url = Bundle.main.url(forResource: "information_" + languageCode, withExtension: "html")  {
+            if let htmlData = try? Data(contentsOf: url )  {
+                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
+     
     }
     
     
-    // Manually load internationalized text for the
-    // textview. It is not automatically loaded from the
-    // storyboard strings file.
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textView.text =
-            NSLocalizedString("instructions_text", comment: "Text shown on the instructions page")
+    // Load links in the about page in the Safari app, instead of the
+    // WebView. This avoids the following problems and work:
+    //    1. App Transport Security issues with http websites
+    //    2. Need to add and program navigation buttons for the webview
+    //
+    //   There was a problem with VoiceOver when connecting to the GitHub
+    //   link in the initial test.
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        if navigationType == UIWebViewNavigationType.linkClicked {
+            UIApplication.shared.openURL(request.url!)
+            return false
+        }
+      
+        return true
     }
    
 }
