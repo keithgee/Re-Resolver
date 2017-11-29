a2b934ffd5cef36a0026e57c1afb1f1976890e0c
Keith Gilbertson <keithg@keithg-schol-2.lib.vt.edu>
2017-11-29 16:20:16 -0500

    Replace UIWebView with WKWebView

--- Re-Resolver/InstructionsViewController.swift
+++ Re-Resolver/InstructionsViewController.swift
@@ -7,45 +7,59 @@
 //
 
 import UIKit
+import WebKit
 
-
-// This class is the controller for the instructions
+// This class is the ViewController for the instructions
 // page.
-class InstructionsViewController: UIViewController, UIWebViewDelegate {
+class InstructionsViewController: UIViewController, WKNavigationDelegate {
 
-    // TODO: UIWebView is deprecated. Replace with WKWebView or
-    // SafariViewController. Note: WKWebView may have problems
+    // Note: WKWebView may have problems
     // when instantiated via storyboards prior to iOS 11.
-    // SafariViewController may (I don't know yet) be difficult
-    // to customize with translucent background rendering to match
-    // the look of the rest of the app.
+    // The storyboard for this scene has a generic UIView
+    // in place to take advantage of the AutoLayout system,
+    // while the main component of the scene, the WKWebView,
+    // is sized to match this UIView and added as a subview.
+    @IBOutlet weak var enclosingView: UIView!
+    var webView: WKWebView! // implicit unwrap seems risky here
     
-    @IBOutlet weak var webView: UIWebView!
     
-    // Load internationalized help text into the webview.
+    // Create and configure the WKWebView and load localized help
+    // text.
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        webView.delegate = self
+        // This could have been done in the storyboard, but
+        // I wanted the view to be visible on the board.
+        enclosingView.backgroundColor = UIColor.clear
+    
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = false
+        webView = WKWebView(frame: enclosingView.frame, configuration: webConfiguration)
+    
+        webView.backgroundColor = UIColor.clear
+        webView.isOpaque = false
+        view.addSubview(webView)
+
+        webView.navigationDelegate = self
         
-        // get the preferred language. This should be more flexible,
+        // Get the preferred language. This should be more flexible,
         // but at present the only localizations are English and Spanish.
         var languageCode = "en"
         let preferredLocalization = Bundle.main.preferredLocalizations[0]
         if preferredLocalization.hasPrefix("es")  {
            languageCode = "es"
         }
-        
+      
         if let url = Bundle.main.url(forResource: "information_" + languageCode, withExtension: "html")  {
-            if let htmlData = try? Data(contentsOf: url )  {
-                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
-                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
-            }
+            webView.loadFileURL(url, allowingReadAccessTo: url)
         }
-     
     }
     
-    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = enclosingView.frame
+    }
+
     // Load links in the about page in the Safari app, instead of the
     // WebView. This avoids the following problems and work:
     //    1. App Transport Security issues with http websites
@@ -53,14 +67,14 @@ class InstructionsViewController: UIViewController, UIWebViewDelegate {
     //
     //   There was a problem with VoiceOver when connecting to the GitHub
     //   link in the initial test.
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
-        if navigationType == UIWebViewNavigationType.linkClicked {
-            UIApplication.shared.openURL(request.url!)
-            return false
+        if navigationAction.navigationType == .linkActivated  {
+            UIApplication.shared.openURL(navigationAction.request.url!)
+            decisionHandler(.cancel)
+        } else  {
+            decisionHandler(.allow)
         }
-      
-        return true
     }
-   
+    
 }
