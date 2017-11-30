576d84d959803a546ed655c5e8c96f26c0c5aace
Keith Gilbertson <keith.gilbertson@gmail.com>
2017-11-29 20:11:39 -0500

    Clean up and document InstructionsViewController

--- Re-Resolver/InstructionsViewController.swift
+++ Re-Resolver/InstructionsViewController.swift
@@ -14,14 +14,17 @@ import WebKit
 class InstructionsViewController: UIViewController, WKNavigationDelegate {
 
     // Note: WKWebView may have problems
-    // when instantiated via storyboards prior to iOS 11.
-    // The storyboard for this scene has a generic UIView
+    // when instantiated via storyboards prior to iOS 11,
+    // but at the time of this writing, the app still functions
+    // under iOS 9.3. Therefore the WKWebView is added from this code
+    // instead of the storyboard.
+    //
+    // The storyboard for this scene has a generic UIView (enclosingView)
     // in place to take advantage of the AutoLayout system,
     // while the main component of the scene, the WKWebView,
-    // is sized to match this UIView and added as a subview.
+    // is added as a subview and sized to fit in viewDIdLayoutSubviews.
     @IBOutlet weak var enclosingView: UIView!
-    var webView: WKWebView! // implicit unwrap seems risky here
-    
+    let webView = WKWebView()
     
     // Create and configure the WKWebView and load localized help
     // text.
@@ -29,17 +32,14 @@ class InstructionsViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         // This could have been done in the storyboard, but
-        // I wanted the view to be visible on the board.
+        // I wanted the enclosingView to be visible in Interface Builder.
         enclosingView.backgroundColor = UIColor.clear
     
-        let webConfiguration = WKWebViewConfiguration()
-        webConfiguration.preferences.javaScriptEnabled = false
-        webView = WKWebView(frame: enclosingView.frame, configuration: webConfiguration)
-    
+        // Configure the webView
+        webView.configuration.preferences.javaScriptEnabled = false
         webView.backgroundColor = UIColor.clear
         webView.isOpaque = false
-        view.addSubview(webView)
-
+        enclosingView.addSubview(webView)
         webView.navigationDelegate = self
         
         // Get the preferred language. This should be more flexible,
@@ -54,19 +54,24 @@ class InstructionsViewController: UIViewController, WKNavigationDelegate {
             webView.loadFileURL(url, allowingReadAccessTo: url)
         }
     }
-    
+
+    // Overridden to set the webView to fill
+    // the bounds of the enclosingView after
+    // layout constraints have been applied
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        webView.frame = enclosingView.frame
+        webView.frame =
+            CGRect(x: 0.0, y: 0.0, width: enclosingView.frame.width, height: enclosingView.frame.height)
     }
 
     // Load links in the about page in the Safari app, instead of the
-    // WebView. This avoids the following problems and work:
+    // WKWebView. This avoids the following problems and work:
     //    1. App Transport Security issues with http websites
     //    2. Need to add and program navigation buttons for the webview
+    //    3. Inconsistencies with previous versions of the app
     //
-    //   There was a problem with VoiceOver when connecting to the GitHub
-    //   link in the initial test.
+    //   TODO: Test with VoiceOver. There was a problem with VoiceOver when connecting to the GitHub
+    //   link in the initial test that used UIWebView instead of WKWebView.
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
         if navigationAction.navigationType == .linkActivated  {
@@ -76,5 +81,4 @@ class InstructionsViewController: UIViewController, WKNavigationDelegate {
             decisionHandler(.allow)
         }
     }
-    
 }
